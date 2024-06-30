75f89f3b8180fa3175dad440aabfa9ed335d42d5
rizwan <118995669+riz-33@users.noreply.github.com>
2024-06-30 19:27:01 +0500

    time

---
+++ web.js
@@ -0,0 +1,9 @@
+let today = new Date ();
+
+let currentHrs = today.getHours();
+
+let currentMins = today.getMinutes();
+
+let currentSecs = today.getSeconds();
+
+console.log (currentHrs, currentMins, currentSecs);
