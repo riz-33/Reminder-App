db178d645667b00679a038765419f5144ae34a44
rizwan <118995669+riz-33@users.noreply.github.com>
2024-07-06 05:40:16 +0500

    update

--- web.js
+++ web.js
@@ -10,8 +10,10 @@ let sec = document.getElementById('sec');
 let date = document.getElementById('date');
 let month = document.getElementById('month');
 let year = document.getElementById('year');
+let day = document.getElementById('day');
+// let period = document.getElementById('period');
 
-console.log (year)
+console.log(year)
 
 setInterval(() => {
     let currentTime = new Date();
@@ -22,7 +24,11 @@ setInterval(() => {
 }, 1000)
 
 let rightNow = new Date();
+let monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+period.innerHTML = (rightNow.getHours() >= 12 ? "PM" : "AM");
 date.innerHTML = (rightNow.getDate() < 10 ? "0" : "") + rightNow.getDate();
-month.innerHTML = (rightNow.getMonth() < 10 ? "0" : "") + rightNow.getMonth();
-year.innerHTML = (rightNow.getFullYear() < 10 ? "0" : "") + rightNow.getFullYear();
+month.innerHTML = monthName[rightNow.getMonth()];
+year.innerHTML = rightNow.getFullYear();
+day.innerHTML = dayName[rightNow.getDay()];
