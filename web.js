1bbb5473a10b8b63021e487cc3659d1cb085d62e
rizwan <118995669+riz-33@users.noreply.github.com>
2024-07-09 18:35:50 +0500

    format change

--- web.js
+++ web.js
@@ -4,39 +4,9 @@ function logout() {
     }, 2000)
 }
 
-// let hrs = document.getElementById('hour');
-// let min = document.getElementById('min');
-// let sec = document.getElementById('sec');
-// let date = document.getElementById('date');
-// let month = document.getElementById('month');
-// let year = document.getElementById('year');
-// let day = document.getElementById('day');
-// let period = document.getElementById('period');
-
-// console.log(year)
-
-// setInterval(() => {
-//     let currentTime = new Date();
-
-//     hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
-//     min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
-//     sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
-// }, 1000)
-
-// let rightNow = new Date();
-// let monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-// let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-// period.innerHTML = (rightNow.getHours() >= 12 ? "PM" : "AM");
-// date.innerHTML = (rightNow.getDate() < 10 ? "0" : "") + rightNow.getDate();
-// month.innerHTML = monthName[rightNow.getMonth()];
-// year.innerHTML = rightNow.getFullYear();
-// day.innerHTML = dayName[rightNow.getDay()];
-
-
 const time = document.querySelector('#time');
-// const date = document.querySelector('#date');
-// const day = document.querySelector('#day');
+const date = document.querySelector('#date');
+const day = document.querySelector('#day');
 
 setInterval(() => {
     let currentTime = new Date();
@@ -63,12 +33,18 @@ setInterval(() => {
     time.textContent = hours + " : " + mins + " : " + secs + " " + period;
 });
 
-// let rightNow = new Date();
+let rightNow = new Date();
+let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-// let today = rightNow.getDate();
-// let month = rightNow.getMonth();
-// let year = rightNow.getFullYear();
+let today = rightNow.getDate();
+let month = rightNow.getMonth();
+let year = rightNow.getFullYear();
+let days = rightNow.getDay();
 
-// date.textContent = today + month + year;
+if (today < 10) {
+    today = "0" + today
+}
 
-// let day = currentTime.getDay();
+date.textContent = today + " " + monthName[month] + " " + year;
+day.textContent = dayName[days];
