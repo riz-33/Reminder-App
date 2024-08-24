26430ac0e6c2b1df1d1010371dd94db8bfcff633
rizwan <118995669+riz-33@users.noreply.github.com>
2024-08-24 07:31:06 +0500

    calendar

--- month.js
+++ month.js
@@ -7,30 +7,39 @@ let currentYear = new Date().getFullYear();
 function fetchPrayerTimes(month, year) {
     fetch(`https://api.aladhan.com/v1/calendar?latitude=24.8607&longitude=67.0011&method=1&school=1&month=${month}&year=${year}`)
         .then(response => response.json())
-        .then(data => renderCalendar(data.data))
+        .then(data => renderTable(data.data))
         .catch(error => console.error('Error fetching prayer times:', error));
 }
 
-function renderCalendar(prayerData) {
-    const month = document.getElementById ('month');
+function renderTable(prayerData) {
     month.innerHTML = monthName[currentMonth - 1] + " " + currentYear;
-    const daysElement = document.getElementById('days');
-    daysElement.innerHTML = ''; // Clear existing days
+    const tbody = document.getElementById('namaz-tbody');
+    tbody.innerHTML = '';
 
     prayerData.forEach(day => {
-        const dayElement = document.createElement('div');
-        dayElement.classList.add('day');
-        dayElement.innerHTML = `
-      <div>${day.date.readable}</div>
-      <div>Fajr: ${day.timings.Fajr}</div>
-      <div>Sunrise: ${day.timings.Sunrise}</div>
-      <div>Dhuhr: ${day.timings.Dhuhr}</div>
-      <div>Asr: ${day.timings.Asr}</div>
-      <div>Maghrib: ${day.timings.Maghrib}</div>
-      <div>Isha: ${day.timings.Isha}</div>
-    `;
-        daysElement.appendChild(dayElement);
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${day.date.readable}</td>
+            <td>${convertTo12Hour(day.timings.Fajr)}</td>
+            <td>${convertTo12Hour(day.timings.Dhuhr)}</td>
+            <td>${convertTo12Hour(day.timings.Asr)}</td>
+            <td>${convertTo12Hour(day.timings.Maghrib)}</td>
+            <td>${convertTo12Hour(day.timings.Isha)}</td>
+        `;
+        tbody.appendChild(tr);
     });
+}
+
+function convertTo12Hour(time) {
+    let cleanTime = time.replace(/ *\([^)]*\) */g, ""); 
+    let [hours, minutes] = cleanTime.split(':');
+    hours = parseInt(hours);
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    return `${hours} : ${minutes} ${ampm}`;
 }
 
 function previousMonth() {
@@ -53,5 +62,4 @@ function nextMonth() {
     fetchPrayerTimes(currentMonth, currentYear);
 }
 
-// Initial call to populate calendar
 fetchPrayerTimes(currentMonth, currentYear);
